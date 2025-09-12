2bba53550a0efb1e7dd3df339b275a079fdcb98e
Sirisha Baddigam <sirishareddybaddigam@gmail.com>
2025-09-12 18:58:31 +0530

    To print a text

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+// To print Any Text on the screen
+
+public class HelloWorld {  
+    //this is the class ,the class name should be file name       
+    //The execution starts here
+
+    public static void main(String[] args) { 
+    //this is the main method where execution starts      
+    //then it will come to main method
+
+        System.out.println("Hello World!");
+        //used to print
+    }
+}
+
+      /* public - Access modifier
+      String[] args (or) String args[] - is an array that holds the command-line arguments */
